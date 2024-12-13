ff0bd94c8af2a5e2574e9823b719b54f693bec90
Gustavo <gusstavocta@gmail.com>
2024-12-13 00:18:28 -0500

    implementando controller series

--- screematch/src/main/java/com/aluracursos/screematch/ScreenmatchApplicationConsole.java
+++ screematch/src/main/java/com/aluracursos/screematch/ScreenmatchApplicationConsole.java
@@ -1,28 +1,28 @@
-package com.aluracursos.screematch;
-
-import com.aluracursos.screematch.principal.Principal;
-import com.aluracursos.screematch.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-@SpringBootApplication
-public class ScreenmatchApplicationConsole implements CommandLineRunner {
-
-    @Autowired
-    private SerieRepository repository;
-
-    public static void main(String[] args) {
-        SpringApplication.run(ScreenmatchApplicationConsole.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        Principal principal = new Principal(repository);
-        principal.muestraElMenu();
-
-
-    }
-}
+//package com.aluracursos.screematch;
+//
+//import com.aluracursos.screematch.principal.Principal;
+//import com.aluracursos.screematch.repository.SerieRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//
+//@SpringBootApplication
+//public class ScreenmatchApplicationConsole implements CommandLineRunner {
+//
+//    @Autowired
+//    private SerieRepository repository;
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ScreenmatchApplicationConsole.class, args);
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        Principal principal = new Principal(repository);
+//        principal.muestraElMenu();
+//
+//
+//    }
+//}
