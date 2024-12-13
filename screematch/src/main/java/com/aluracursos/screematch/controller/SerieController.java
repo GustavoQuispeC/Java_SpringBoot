e805cac92e25df69aa209d40c7c02a4fb2fc2b60
Gustavo <gusstavocta@gmail.com>
2024-12-13 01:16:33 -0500

    implementando politicas CORS

--- screematch/src/main/java/com/aluracursos/screematch/controller/SerieController.java
+++ screematch/src/main/java/com/aluracursos/screematch/controller/SerieController.java
@@ -1,13 +1,32 @@
 package com.aluracursos.screematch.controller;
 
+import com.aluracursos.screematch.dto.SerieDTO;
+import com.aluracursos.screematch.model.Serie;
+import com.aluracursos.screematch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repository;
+
     @GetMapping("/series")
-    public String getSeries() {
-        return "Este es mi primer servicio web";
+    public List<SerieDTO> obtenerTodasLasSeries() {
+        return repository.findAll().stream()
+                .map(s -> new SerieDTO(s.getTitulo(), s.getTotalTemporadas(),
+                        s.getEvaluacion(), s.getPoster(), s.getGenero().toString(),
+                        s.getActores(), s.getSinopsis())).collect(Collectors.toList());
+    }
+
+    @GetMapping("/inicio")
+    public  String muestraMensaje() {
+        return "Hola, bienvenido a ScreenMatch";
     }
 }
---
+++ screematch/src/main/java/com/aluracursos/screematch/dto/SerieDTO.java
@@ -0,0 +1,14 @@
+package com.aluracursos.screematch.dto;
+
+public record SerieDTO(
+        String titulo,
+        Integer totalTemporadas,
+        Double evaluacion,
+        String poster,
+        String genero,
+        String actores,
+        String sinopsis
+) {
+
+
+}
